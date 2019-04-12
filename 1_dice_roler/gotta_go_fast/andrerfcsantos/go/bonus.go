88ac2f2dc23f848eeaa0f6b6a9f0d32df09961d8
André Santos <andrerfcsantos@gmail.com>
2019-04-12 16:16:12 +0100

    Pull request cleaning

--- 1_dice_roler/gotta_go_fast/andrerfcsantos/go/bonus.go
+++ 1_dice_roler/gotta_go_fast/andrerfcsantos/go/bonus.go
@@ -2,60 +2,14 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
-	"log"
 	"math/rand"
 	"os"
-	"runtime/pprof"
-	"runtime/trace"
 	"strconv"
 	"strings"
 )
 
-var tracerEnabled bool
-var cpuProfileEnabled bool
-
-func parseFlags() {
-	flag.BoolVar(&tracerEnabled, "trace", false, "")
-	flag.BoolVar(&cpuProfileEnabled, "cpu", false, "")
-	flag.Parse()
-}
-
 func main() {
-	parseFlags()
-	if cpuProfileEnabled {
-		fp, err := os.Create("bonuscpu.prof")
-		if err != nil {
-			log.Fatal("could not create CPU profile: ", err)
-		}
-		defer fp.Close()
-		if err := pprof.StartCPUProfile(fp); err != nil {
-			log.Fatal("could not start CPU profile: ", err)
-		}
-		defer pprof.StopCPUProfile()
-
-	}
-
-	if tracerEnabled {
-		f, err := os.Create("bonustrace.out")
-		if err != nil {
-			log.Fatalf("failed to create trace output file: %v", err)
-		}
-		defer func() {
-			if err := f.Close(); err != nil {
-				log.Fatalf("failed to close trace file: %v", err)
-			}
-		}()
-
-		if err := trace.Start(f); err != nil {
-			log.Fatalf("failed to start trace: %v", err)
-		}
-		defer trace.Stop()
-	}
-
-	//f, _ := os.Open("../../../input.txt")
-	//scanner := bufio.NewScanner(f)
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
@@ -75,8 +29,5 @@ func main() {
 			fmt.Printf(" %d", r)
 		}
 		fmt.Print("\n")
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
 }
--- 1_dice_roler/gotta_go_fast/andrerfcsantos/go/main.go
+++ 1_dice_roler/gotta_go_fast/andrerfcsantos/go/main.go
@@ -2,61 +2,14 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
-	"log"
 	"math/rand"
 	"os"
-	"runtime/pprof"
-	"runtime/trace"
 	"strconv"
 	"strings"
 )
 
-var tracerEnabled bool
-var cpuProfileEnabled bool
-
-func parseFlags() {
-	flag.BoolVar(&tracerEnabled, "trace", false, "")
-	flag.BoolVar(&cpuProfileEnabled, "cpu", false, "")
-	flag.Parse()
-}
-
 func main() {
-	parseFlags()
-	if cpuProfileEnabled {
-		fp, err := os.Create("maincpu.prof")
-		if err != nil {
-			log.Fatal("could not create CPU profile: ", err)
-		}
-		defer fp.Close()
-		if err := pprof.StartCPUProfile(fp); err != nil {
-			log.Fatal("could not start CPU profile: ", err)
-		}
-		defer pprof.StopCPUProfile()
-
-	}
-
-	if tracerEnabled {
-		f, err := os.Create("maintrace.out")
-		if err != nil {
-			log.Fatalf("failed to create trace output file: %v", err)
-		}
-		defer func() {
-			if err := f.Close(); err != nil {
-				log.Fatalf("failed to close trace file: %v", err)
-			}
-		}()
-
-		if err := trace.Start(f); err != nil {
-			log.Fatalf("failed to start trace: %v", err)
-		}
-		defer trace.Stop()
-	}
-
-	//f, _ := os.Open("../../../input.txt")
-	//scanner := bufio.NewScanner(f)
-
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
@@ -69,8 +22,5 @@ func main() {
 			sum += rand.Intn(d-1) + 1
 		}
 		fmt.Println(d)
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
 }
