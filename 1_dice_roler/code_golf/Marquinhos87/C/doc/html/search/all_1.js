b35b4bf7c113f5f54b17c99f4bd9bdbcbeba0271
Marquinhos87 <joaomarques8713@gmail.com>
2019-10-02 16:10:32 +0100

    dice roler terminado

---
+++ 1_dice_roler/code_golf/Marquinhos87/C/doc/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['diceroler_2eh',['diceroler.h',['../diceroler_8h.html',1,'']]]
+];
---
+++ 1_dice_roler/code_golf/Marquinhos87/C/doc/html/search/functions_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['prt',['prt',['../diceroler_8h.html#a784c2f0f86fdcaf4f18a53f33ec36c76',1,'diceroler.c']]]
+];
