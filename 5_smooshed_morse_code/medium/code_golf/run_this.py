11c3f97cded249c3ec8275ca41719488f313ca3b
Mata <pmmatarodrigues@gmail.com>
2019-10-02 23:36:07 +0100

    add main function

--- 5_smooshed_morse_code/medium/code_golf/run_this.py
+++ 5_smooshed_morse_code/medium/code_golf/run_this.py
@@ -2,5 +2,12 @@
 
 letters = "abcdefghijklmnopqrstuwvxyz"
 morse = ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..".split()
+# CREATE DICTIONARY WITH LETTERS AND MORSE
 dictionary = dict(zip(morse, letters))
 
+
+
+def check_morse_code(original_code):
+    print(original_code)
+
+print(check_morse_code('-----......------.--.-...-.---.-..-.....-.-.-.-..--..-.--.--..-..-...-..--.....--.'))
