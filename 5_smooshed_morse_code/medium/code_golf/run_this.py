0a1787b547756caf2fb78842ab507e6e3f84ccc0
Mata <pmmatarodrigues@gmail.com>
2019-10-02 23:06:45 +0100

    Add test_input and python main script

---
+++ 5_smooshed_morse_code/medium/code_golf/run_this.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+letters = "abcdefghijklmnopqrstuwvxyz"
+morse = ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..".split()
+dictionary = dict(zip(morse, letters))
+
