6630b6bad6c48a47a841316bdb2d8fe199de9454
Mata <pmmatarodrigues@gmail.com>
2019-10-06 18:23:35 +0100

    Minor changes to variable's names and algorithm

--- 5_smooshed_morse_code/medium/code_golf/run_this.py
+++ 5_smooshed_morse_code/medium/code_golf/run_this.py
@@ -4,24 +4,30 @@ letters = "abcdefghijklmnopqrstuwvxyz"
 morse = ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..".split()
 # CREATE DICTIONARY WITH LETTERS AND MORSE
 dictionary = dict(zip(morse, letters))
+result = []
+
+def check_morse_code(original_code, all = set(dictionary), used_letters = []):
 
 
-
-def check_morse_code(original_code, dictionary, used_letters = []):
-    
-    for i in range(0,3):
+    for i in range(1,5):
         checking = original_code[:i]
 
-        if checking in dictionary:            
+        if checking in all:            
             used_letter = dictionary.get(checking, "")
 
-            dictionary.pop(checking)
-            print(checking)
-            print(used_letter, end=" ")
+            used_letters.append(checking)
+            # REMOVE USED LETTERS FROM DICTIONARY
+            rest = all.copy()
+            rest.remove(checking)
+            result.append(used_letter)
 
-            check_morse_code(original_code[i:], dictionary)
+            # CHECK IF LETTERS ARE BEING REMOVED
+
+            check_morse_code(original_code[i:], rest)
+            break    
 
 
 
-check_morse_code('.--...-.-.-.....-.--........----.-.-..---.---.--.--.-.-....-..-...-.---..--.----..', dictionary)
+check_morse_code('.--...-.-.-.....-.--........----.-.-..---.---.--.--.-.-....-..-...-.---..--.----..')
+print(result)
 
