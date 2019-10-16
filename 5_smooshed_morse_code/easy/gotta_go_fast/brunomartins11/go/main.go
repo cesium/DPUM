2121a323493a0022f10eaf78bd2919c8843e0def
BrunoMartins11 <brunocmartins@live.com.pt>
2019-10-16 16:07:00 +0100

    Go fast easy challenge 5  (#17)

---
+++ 5_smooshed_morse_code/easy/gotta_go_fast/brunomartins11/go/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+  "bufio"
+  "fmt"
+  "os"
+  "sync"
+  "runtime/debug"
+)
+
+
+var wg sync.WaitGroup
+
+func smosh(line string, m map[string]string) {
+  code := ""
+  for i := 0; i < len(line); i++ {
+    code = code + m[string(line[i])]
+  }
+  fmt.Println(code)
+  wg.Done()
+}
+
+func main() {
+
+  debug.SetGCPercent(-1)
+  m  := map[string]string{
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+  }
+
+  scanner := bufio.NewScanner(os.Stdin)
+
+  for scanner.Scan() {
+    line := scanner.Text()
+    wg.Add(1)
+    go smosh(line, m)
+  }
+  wg.Wait()
+}
